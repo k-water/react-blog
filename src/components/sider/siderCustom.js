cb521859da23a5c9e64e6c0fd4f54e5236118758
k-water <625592890@qq.com>
2018-02-08 20:12:19 +0800

    feat: add admin info

--- src/components/sider/siderCustom.js
+++ src/components/sider/siderCustom.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
+import avatar from '../../assets/avatar.jpg'
 import {
   Card
 } from 'antd'
@@ -17,20 +18,33 @@ class SiderCustom extends Component {
   render() {
     return (
       <div className="sider-container">
-        <Card title="最近文章" bordered={false}>
-          {
-            this.props.content ? <ul className="recent-list">
-              {
-                this.props.content.map(v => (
-                    <li key={v.id} onClick={() => this.props.history.push(`/app/blog/desc/${v.id}`)}>
-                      {v.title}
-                    </li>
-                ))
-              }
-            </ul>
-            : null
-          }
-        </Card>
+        <div className="admin-info">
+          <header>
+            <img src={avatar} alt="avatar"/>
+          </header>
+          <p className="admin-name">
+            Water
+          </p>
+          <p className="admin-desc">
+          二次元，业余摄影，前端打杂人员，略微代码洁癖
+          </p>
+        </div>
+        <div className="recent-article">
+          <Card title="最近文章" bordered={false}>
+            {
+              this.props.content ? <ul className="recent-list">
+                {
+                  this.props.content.map(v => (
+                      <li key={v.id} onClick={() => this.props.history.push(`/app/blog/desc/${v.id}`)}>
+                        {v.title}
+                      </li>
+                  ))
+                }
+              </ul>
+              : null
+            }
+          </Card>
+        </div>
       </div>
     )
   }
