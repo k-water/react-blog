bb0b2ac6a1fb7917a0d07e6b8eb09b521a663415
k-water <625592890@qq.com>
2018-03-18 14:15:24 +0800

    feat: add sider tags

--- src/components/sider/siderCustom.js
+++ src/components/sider/siderCustom.js
@@ -2,18 +2,41 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import avatar from '../../assets/avatar.jpg'
+import axios from 'axios'
 import {
-  Card
+  Card,
+  Tag
 } from 'antd'
 import './sider.css'
+import { color } from '../../utils/utils'
 @withRouter
 @connect(
   state => state.blog,
   {}
 )
 class SiderCustom extends Component {
+  constructor() {
+    super()
+    this.state = {
+      tags: []
+    }
+    this.getTags = this.getTags.bind(this)
+  }
   componentDidMount() {
-
+    this.getTags()
+  }
+  getTags() {
+    axios.get('/api/tags')
+      .then(res => {
+        if (res.status === 200 && res.data.code === 0) {
+          this.setState({
+            tags: res.data.tags
+          })
+        }
+      })
+      .catch(err => {
+        throw(err)
+      })
   }
   render() {
     return (
@@ -26,7 +49,7 @@ class SiderCustom extends Component {
             Water
           </p>
           <p className="admin-desc">
-          二次元，业余摄影，前端打杂人员，略微代码洁癖
+            二次元，业余摄影，前端打杂人员，略微代码洁癖
           </p>
         </div>
         <div className="recent-article">
@@ -43,6 +66,25 @@ class SiderCustom extends Component {
               </ul>
               : null
             }
+          </Card>
+        </div>
+        <div className="tags-wrapper">
+          <Card title="标签" bordered={false}>
+            <div className="tags-content">
+              {
+                this.state.tags ?
+                  this.state.tags.map(v => (
+                    <Tag
+                      key = {v}
+                      color={color[Math.floor(Math.random()*color.length)]}
+                      onClick={()=>this.props.history.push(`/app/tags/${v}`)}
+                    >
+                      {v}
+                    </Tag>
+                  ))
+                : null
+              }
+            </div>
           </Card>
         </div>
       </div>
